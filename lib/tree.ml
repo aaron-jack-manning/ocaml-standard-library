095d9081e123fd4a9f0d4e823ea91041d7036ae5
aaron-jack-manning <aaronjackmanning1@gmail.com>
2021-12-17 07:23:03 +1100

    tree map

--- lib/tree.ml
+++ lib/tree.ml
@@ -1,5 +1,10 @@
-open List
+open FromStdlib
 open Types
 
 let combine (tr1 : 'a tree) (tr2 : 'a tree) (topBranch : 'a) : 'a tree =
     Branch (topBranch, tr1 :: tr2 :: [])
+
+let rec map (f : 'a -> 'b) (tr : 'a tree) : 'b tree =
+    match tr with
+    | Branch (value, branches) -> Branch (f value, branches |> List.map (map f))
+    | Leaf -> Leaf
